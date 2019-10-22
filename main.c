afb93ca34012a1b2b994ea871001a0370381ebbe
Tristan BONNESON <trbonnes@e2r3p14.42.fr>
2019-10-22 12:13:01 +0200

    main

---
+++ main.c
@@ -0,0 +1,66 @@
+#include "ft_printf.h"
+#include <stdio.h>
+
+int	main(void)
+{
+	int ft;
+	int true;
+	char *ptr;
+
+	ptr = malloc(1);
+	printf("\n///c///\n");
+	ft = ft_printf("ft: %c %5c %-4c\n", 'a', '	', '7');
+	true = printf("pf: %c %5c %-4c\n", 'a', '	', '7');
+	printf("ft: %d\n", ft);
+	printf("pf: %d\n", true);
+	printf("\n");
+	printf("///s///\n");
+	ft = ft_printf("ft: %s %10s %-8s\n", "Bonjour", "Tristan", "42");
+	true = printf("pf: %s %10s %-8s\n", "Bonjour", "Tristan", "42");
+	printf("ft: %d\n", ft);
+	printf("pf: %d\n", true);
+	printf("\n");
+	printf("///p///\n");
+	ft = ft_printf("ft: %p %15p %-15p\n", ptr, ptr, ptr);
+	true = printf("pf: %p %15p %-15p\n", ptr, ptr, ptr);
+	printf("ft: %d\n", ft);
+	printf("pf: %d\n", true);
+	printf("\n");
+	printf("///d///\n");
+	ft = ft_printf("ft: %d %7d %-9d %05d\n", 256, 1024, 0, 29);
+	true = printf("pf: %d %7d %-9d %05d\n", 256, 1024, 0, 29);
+	printf("ft: %d\n", ft);
+	printf("pf: %d\n", true);
+	printf("\n");
+	printf("///i///\n");
+	ft = ft_printf("ft: %i %7i %-9i %05i\n", 256, 1024, 0, 29);
+	true = printf("pf: %i %7i %-9i %05i\n", 256, 1024, 0, 29);
+	printf("ft: %d\n", ft);
+	printf("pf: %d\n", true);
+	printf("\n");
+	printf("///u///\n");
+	ft = ft_printf("ft: %u %7u %-9u %05u\n", 256, 1024, 0, 29);
+	true = printf("pf: %u %7u %-9u %05u\n", 256, 1024, 0, 29);
+	printf("ft: %d\n", ft);
+	printf("pf: %d\n", true);
+	printf("\n");
+	printf("///x///\n");
+	ft = ft_printf("ft: %x %7x %-9x %05x\n", 256, 1024, 0, 29);
+	true = printf("pf: %x %7x %-9x %05x\n", 256, 1024, 0, 29);
+	printf("ft: %d\n", ft);
+	printf("pf: %d\n", true);
+	printf("\n");
+	printf("///X///\n");
+	ft = ft_printf("ft: %X %7X %-9X %05X\n", 256, 1024, 0, 29);
+	true = printf("pf: %X %7X %-9X %05X\n", 256, 1024, 0, 29);
+	printf("ft: %d\n", ft);
+	printf("pf: %d\n", true);
+	printf("\n");
+	printf("///%%///\n");
+	ft = ft_printf("ft: %%\n");
+	true = printf("pf: %%\n");
+	printf("ft: %d\n", ft);
+	printf("pf: %d\n", true);
+	printf("\n");
+	return (0);
+}
