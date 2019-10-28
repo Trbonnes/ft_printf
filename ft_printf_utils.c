ca911ae33404cb30be396c0e23a0f5f43c7deb74
Tristan BONNESON <trbonnes@e2r3p14.42.fr>
2019-10-28 15:20:12 +0100

    OK

--- ft_printf_utils.c
+++ ft_printf_utils.c
@@ -6,7 +6,7 @@
 /*   By: trbonnes <trbonnes@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/22 08:51:27 by trbonnes          #+#    #+#             */
-/*   Updated: 2019/10/28 14:55:16 by trbonnes         ###   ########.fr       */
+/*   Updated: 2019/10/28 15:14:23 by trbonnes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,12 +39,12 @@ void	ft_displaypres(size_t **flag, size_t i, int bool, int neg)
 	flag[0][1] >= flag[2][1] && flag[0][1] > i)
 		while (l++ < (flag[0][1] - flag[2][1] - neg))
 			ft_putchar_fd(' ', 1);
-	if (!bool && neg)
-		ft_putchar_fd('-', 1);
 	l = 0;
 	if (!bool && flag[1][0] && flag[1][1] >= flag[2][1] && flag[1][1] > i)
-		while (l++ < (flag[1][1] - flag[2][1]))
+		while (l++ < (flag[1][1] - flag[2][1] - neg))
 			ft_putchar_fd(' ', 1);
+	if (!bool && neg)
+		ft_putchar_fd('-', 1);
 	l = 0;
 	if (!bool && flag[2][1] > i)
 		while (l++ < (flag[2][1] - i))
