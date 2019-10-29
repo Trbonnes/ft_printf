909849bfeaede368c09e3f4ce142c2aab4f2cddd
Tristan BONNESON <trbonnes@e2r3p14.42.fr>
2019-10-29 17:56:52 +0100

    precision f

--- ft_double.c
+++ ft_double.c
@@ -6,7 +6,7 @@
 /*   By: trbonnes <trbonnes@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/29 13:35:37 by trbonnes          #+#    #+#             */
-/*   Updated: 2019/10/29 17:24:09 by trbonnes         ###   ########.fr       */
+/*   Updated: 2019/10/29 17:55:13 by trbonnes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,8 @@ int		ft_doublef(va_list *ap, int r_value, size_t **flag)
 	double		tmp;
 	long long	conv;
 	long long	conv_tmp;
-	int			i;
-	int 		j;
+	size_t		i;
+	size_t		j;
 	int			neg;
 
 	f = va_arg(*ap, double);
@@ -48,16 +48,37 @@ int		ft_doublef(va_list *ap, int r_value, size_t **flag)
 		conv *= -1;
 	}
 	str = ft_dtos(conv, i);
-	ft_putstr_fd(str, 1);
 	i = 0;
 	j = 0;
 	while (str[i] != '.')
 		i++;
 	while (str[i + j])
 		j++;
-	if (j <= 6)
-		while (j++ <= 6)
-			ft_putchar_fd('0', 1);
-	r_value = ft_strlen(str);
-	return (r_value);
+	if (flag[2][0])
+	{
+		if (j <= flag[2][1])
+		{
+			i = ft_strlen(str);
+			ft_putstr_fd(str, 1);
+			while (j++ <= flag[2][1] && i++)
+				ft_putchar_fd('0', 1);
+		}
+		else
+		{
+			j = flag[2][1];
+			if (str[i + j + 1] >= 5)
+				str[i + j]++;
+			str[i + j + 1] = '\0';
+			ft_putstr_fd(str, 1);
+		}
+	}
+	else
+	{
+		i = ft_strlen(str);
+		ft_putstr_fd(str, 1);
+		if (j <= 6)
+			while (j++ <= 6 && i++)
+				ft_putchar_fd('0', 1);
+	}
+	return (r_value + i);
 }
