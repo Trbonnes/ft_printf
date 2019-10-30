3283a30d31cb46a338ce0ee9f9902ad9effa508d
Tristan BONNESON <trbonnes@e2r3p14.42.fr>
2019-10-30 15:23:54 +0100

    new file

--- ft_indiclong.c
+++ ft_indiclong.c
@@ -6,7 +6,7 @@
 /*   By: trbonnes <trbonnes@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/24 13:59:19 by trbonnes          #+#    #+#             */
-/*   Updated: 2019/10/29 11:15:55 by trbonnes         ###   ########.fr       */
+/*   Updated: 2019/10/30 15:23:08 by trbonnes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -99,38 +99,4 @@ int		ft_printlu(va_list *ap, int r_value, size_t **flag)
 	if (length > i)
 		return (r_value + (int)length);
 	return (r_value + (int)i);
-}
-
-int	ft_printlx(va_list *ap, int r_value, int maj, size_t **flag)
-{
-	char	*str;
-	size_t	nb;
-	size_t	i;
-	size_t	length;
-
-	i = -1;
-	nb = (size_t)va_arg(*ap, unsigned long int);
-	str = ft_convert_base(nb);
-	if (maj == 1)
-		while (str[++i])
-			str[i] = ft_toupper((int)str[i]);
-	i = ft_strlen(str);
-	ft_fielddisplay(flag, i, 0, 0);
-	if (flag[3][0] && i++ && i++)
-		ft_putstr_fd("0x", 1);
-	ft_putstr_fd(str, 1);
-	ft_fielddisplay(flag, i, 1, 0);
-	length = ft_length(flag, 0);
-	if (length > i)
-		return (r_value + (int)length);
-	return (r_value + (int)i);
-}
-
-int		ft_println(va_list *ap, int r_value)
-{
-	long long int *n;
-
-	n = va_arg(*ap, long long int *);
-	*n = r_value;
-	return (r_value);
 }
